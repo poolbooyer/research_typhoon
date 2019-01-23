ef1c9738d0ea3dadd50fc71ab9543356bf319355
Niioka <ryou3018@gmail.com>
2019-01-23 11:45:18 +0900

    コメントの追加,リファクタリング

--- checker/serch.rb
+++ checker/serch.rb
@@ -1,21 +1,23 @@
-s = []
+#データを読み込み
+read_data = []
 File.open("data/output/part-r-00000", mode = "rt"){|f|
-  s = f.read
+  read_data = f.read
 }
-s=s.split("\n")
+#読み込んだデータを改行コードで配列に格納
+read_data=read_data.split("\n")
 data=[]
-s.each do |line|
+#配列をtab空白単位で分割
+read_data.each do |line|
     data.push(line.split("\t"))
 end
+#各要素の型変換を実施
 i=0
-j=0
 data.each do|line|
-    line.each do|cel|
-        data[i][j]=cel.to_i
-        j=j+1
-    end
+    data[i][0]=line[0].to_i
+    data[i][1]=line[1].to_f
+    data[i][2]=line[2].to_i
     i=i+1
-    j=0
 end
+#データを期間の昇順にソート
 data_sort=data.sort{|a, b|a[2] <=> b[2]}
 p data_sort
